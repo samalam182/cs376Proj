aa943edcd009e86dabd8ead22814a4c9aa2f85e6
Mitchell <estes17@up.edu>
2014-10-09 00:31:22 +0000

    created username function for estes17 favorite food

---
+++ estes17.c
@@ -0,0 +1,8 @@
+/* this is file estes17.c */
+#include <stdio.h>
+#include "estes17.h"
+
+void estes17() {
+  //replace the following print statement with your favorite food.
+  printf("estes17 likes metals with lots of french fries.\n"); 
+}
